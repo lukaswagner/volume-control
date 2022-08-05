53c65041b37c2c604c6677e770f0f7c4f9f6b271
Lukas Wagner <lukas@lwgnr.dev>
2022-08-05 14:30:37 +0200

    map nt paths to dos paths

--- include/helper.hpp
+++ include/helper.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <map>
 #include <stdexcept>
 #include <string>
 
@@ -9,6 +10,7 @@
 std::string toString(LPWSTR wstr);
 std::string replaceEnvVars(std::string str);
 std::string getPath(std::string str);
+std::string toDosPath(std::string path);
 
 #define CHECK(status, message)                                                 \
     if (status != S_OK) throw std::runtime_error(message);
--- src/helper.cpp
+++ src/helper.cpp
@@ -2,6 +2,8 @@
 
 #include <regex>
 
+#include <fileapi.h>
+
 std::string toString(LPWSTR wstr)
 {
     auto len = wcstombs(NULL, wstr, 0);
@@ -40,9 +42,64 @@ std::string replaceEnvVars(std::string str)
 
 std::string getPath(std::string str)
 {
+    std::string result = "";
     auto replaced = replaceEnvVars(str);
     std::regex path(R"((C:)?\\.*\\\w+\.\w+)");
     std::smatch match;
-    if (std::regex_search(replaced, match, path)) return match.str();
-    return "";
+    if (std::regex_search(replaced, match, path)) result = match.str();
+    if (result.at(0) == '\\') result = toDosPath(result);
+    return result;
+}
+
+std::map<std::string, std::string> deviceMap;
+void buildDeviceMap()
+{
+    deviceMap.clear();
+
+    std::string inStr;
+    std::string outStr;
+
+    std::wstring inWideStr;
+    std::wstring outWideStr;
+
+    const size_t inChars = 256;
+    size_t outChars;
+
+    wchar_t outWideCStr[inChars];
+
+    for (char c = 'C'; c <= 'Z'; ++c)
+    {
+        inStr = std::string(1, c).append(":");
+        inWideStr = std::wstring(inStr.begin(), inStr.end());
+        outChars = QueryDosDeviceW(inWideStr.c_str(), outWideCStr, inChars);
+        if (outChars == 0)
+        {
+            auto err = GetLastError();
+            continue;
+        }
+        outWideStr = std::wstring(outWideCStr);
+        if (outWideStr.length() == 0) continue;
+        outStr = std::string(outWideStr.begin(), outWideStr.end());
+        deviceMap[outStr] = inStr;
+    }
+
+#if false
+    std::cerr << "### device map ###" << std::endl;
+    for (auto&& p : deviceMap)
+    {
+        std::cerr << p.first << " -> " << p.second << std::endl;
+    }
+#endif
+}
+
+std::string toDosPath(std::string path)
+{
+    if (deviceMap.size() == 0) buildDeviceMap();
+    for (auto&& p : deviceMap)
+    {
+        auto start = path.find(p.first);
+        if (start != -1)
+            path.replace(start, start + p.first.length(), p.second);
+    }
+    return path;
 }
